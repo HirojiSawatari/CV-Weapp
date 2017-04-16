5fe043a4aa4e779267ccc7eec86a6d0231be0369
HirojiSawatari <ka_tou@outlook.com>
2017-04-16 11:51:04 +0800

    Add a map in the contact

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -5,7 +5,14 @@ Page({
       phone: '186-5562-9450',
       email: 'ka_tou@katouspace.com',
       qrcode: '../../images/qrcode.jpg'
-    }
+    },
+    latitude: 39.992617,
+    longitude: 116.317001,
+    markers: [{
+      latitude: 39.992617,
+      longitude: 116.317001,
+      name: '北京大学物理学院\n北京市海淀区成府路209号'
+    }],
   },
   onLoad () { 
     var that = this 
